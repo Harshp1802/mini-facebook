3b0afaf319710e31f54b7c4a9577f60eb48c6ef8
viraj2707 <viraj.shah@iitgn.ac.in>
2020-11-11 17:29:20 +0530

    sorting out some things on friend requests

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ DATABASE = {"user1":
                 "socket_details": "",
                 "is_online": False,
                 "friends": [],
+                "pending_friend_requests = []",
                 "posts_visible_friends": [],
                 "posts_global": [],
                 "posts_private": [],
@@ -33,9 +34,34 @@ f.close()
 def home_screen(username, socket_client):
     
     while(True):
+        user_home = "Welcome to Mini-Face:\n"
+        f_reqs = DATABASE[username]["pending_friend_requests"]
+        if(f_reqs):    
+            user_home += "Pending Friend Requests:\n"
+            for i in range(len(f_reqs)):
+                each = str(i+1) + ". " + f_reqs[i] + "\n"
+                user_home += each
+            user_home += "Select a request, 0 to ignore all\n"
+            socket_client.send(user_home)
+            r_no = socket_client.recv(1024)
+            if(r_no!=0):
+                response = str(r_no) + ". " + f_reqs[r_no-1] + "\n"
+                response += "Accept(y) or Delete(n)"
+                socket_client.send(response)
+                ans = socket_client.recv(1024)
+                
+                if(ans=='y'):
+                    DATABASE[username]['friends'].append(f_reqs[r_no])
+                    DATABASE[f_reqs[r_no]]['friends'].append(username)
+                DATABASE[username]['pending_friend_requests'].remove(f_reqs)
+        
+        home_options(username, socket_client)
+
+def home_options(username, socket_client):
+    while(True):
         socket_client.send(
             """
-            Welcome to Mini-Face: (Reply with)
+            Options: (Reply with)
             1: Friends
             2: Messages
             0: Exit Mini-Face
@@ -67,14 +93,29 @@ def find_friend(username, socket_client):
             query = socket_client.recv(1024)
             response = "Search Results: \n"
             count = 0
+            search_result = []
             for i in user_list:
                 if i.find(query) != -1:
-                    each = str(count+1) + ". " + i + "\n"
-                    response += each
-                    count+=1
+                    search_result.append(i)
+            search_result = np.array(search_result)
+            self_index = np.argwhere(search_result==username)
+            search_result = np.delete(search_result,self_index)         # delete self
+            search_result = search_result - np.array(DATABASE[username]['friends']) # delete client's friends
+            
+            for i in search_result:
+                each = str(count+1) + ". " + i + "\n"
+                response += each
+                count+=1
             if(count == 0):
                 response = "No results found"
+            else:
+                response += "Enter number to send friend request\n"
             socket_client.send(response)
+            friend_number = socket_client.recv(1024)
+
+            if username not in DATABASE[ search_result[friend_number-1] ]['pending_friend_requests']:       # check if already exists
+                DATABASE[ search_result[friend_number-1] ]['pending_friend_requests'].append(username)
+
         if(option == "2"):
             if(len(DATABASE[username]['friends'] < 2)):
                 response = "Make more friends!"
@@ -88,6 +129,18 @@ def find_friend(username, socket_client):
                 fof = np.unique(fof)
                 self_index = np.argwhere(fof==username)
                 fof = np.delete(fof,self_index)         # delete self
+                fof = fof - np.array(DATABASE[username]['friends']) # delete client's friends
+
+                response = "Results:\n"
+                for i in range(len(fof)):
+                    each = str(i+1) + ". " + fof[i] + "\n"
+                    response += each
+                response += "Enter number to send friend request\n"
+                socket_client.send(response)
+                friend_number = socket_client.recv(1024)
+
+                if username not in DATABASE[ fof[friend_number-1] ]['pending_friend_requests']:     # check if already exists
+                    DATABASE[ fof[friend_number-1] ]['pending_friend_requests'].append(username)
     
 
 def friend_options(username, socket_client):
@@ -97,6 +150,7 @@ def friend_options(username, socket_client):
             Friend Options        
             1: See your Friends
             2: Find new Friends
+            3: Remove Friends
             0: Go to Home Screen
             """)
         option = socket_client.recv(1024)
@@ -112,13 +166,15 @@ def friend_options(username, socket_client):
                     status = "Away"
                 each = str(i+1) + ". " + friend_list[i] + ":\t" + status + "\n"
                 response += each
+            response += "Enter a key to go back"
             socket_client.send(response)
+            a = socket_client.recv(1024)
         if(option == "2"):
             find_friend(username, socket_client)
 
 
 def check_username(username):
-    if(username in DATABASE.keys()):
+    if(username in user_list):
         return(0)
     return(1)
 def add_client(username,password):
